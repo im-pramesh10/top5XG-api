907fc66208900b586a8b67f5abc49c9a0aa407dc
Pramesh Mahat <cruisepramesh@gmail.com>
2023-02-15 17:32:19 +0545

    code update

--- updatePlayerDetails.js
+++ updatePlayerDetails.js
@@ -1,55 +1,54 @@
-let arr=[];
-const fs = require('fs');
+function main() {
+    let arr = [];
+    const fs = require('fs');
 
-fetch('https://fantasy.premierleague.com/api/bootstrap-static/')
-    .then(res=>res.json())
-    .then(data=> {
+    fetch('https://fantasy.premierleague.com/api/bootstrap-static/').then(res => res.json()).then(data => {
         loopPlayers(data);
     })
 
-function loopPlayers(data) {
-    let lengthCheck=data.elements.length;
-    for(let i=0;i<data.elements.length;i++) {
-        setTimeout(() => {
-            fetch(`https://fantasy.premierleague.com/api/element-summary/${data.elements[i].id}/`)
-            .then(res=>res.json())
-            .then(data2=> {
-                writetofile(lengthCheck, data2, data.elements[i].id, data.elements[i].web_name, data.teams[data.elements[i].team-1].short_name);
-            })
-            .catch(err=> {
-                console.log(err);
+    function loopPlayers(data) {
+        let lengthCheck = data.elements.length;
+        for (let i = 0; i < data.elements.length; i++) {
+            fetch(`https://fantasy.premierleague.com/api/element-summary/${
+                data.elements[i].id
+            }/`).then(res => res.json()).then(data2 => {
+                writetofile(lengthCheck, data2, data.elements[i].id, data.elements[i].web_name, data.teams[data.elements[i].team - 1].short_name);
+            }).catch(err => {
+                console.error(err);
             });
-        }, 400);       
+
+        }
+    }
+
+    function writetofile(lengthCheck, data2, id, name, team) {
+        // console.log(data2);
+        // console.log("hello"+i);
+        let newdata = {
+            "id": id,
+            "name": name + "(" + team + ")",
+            ... data2
+        };
+        arr.push(newdata);
+        console.log("Below num should be equal to " + lengthCheck);
+        console.log(arr.length);
+        if (arr.length === lengthCheck) {
+            writeArray();
+        }
+    }
+
+    function writeArray() {
+
+        try { // convert JSON object to a string
+            const arrjson = JSON.stringify(arr, null, 4)
+
+            // write file to disk
+            fs.writeFileSync('./playerDetailsArray.json', arrjson, 'utf8')
+
+            console.log(`File is written successfully!`)
+        } catch (err) {
+            console.log(`Error writing file: ${err}`)
+        }
     }
 }
 
-function writetofile(lengthCheck, data2, id, name, team) {
-    // console.log(data2);
-    // console.log("hello"+i);
-    let newdata = {
-        "id": id,
-        "name": name+"("+team+")",
-        ...data2
-    };
-    arr.push(newdata);
-    console.log("Below num should be equal to "+lengthCheck);
-    console.log(arr.length);
-    if (arr.length===lengthCheck){
-        writeArray();
-    }
-}
-
-function writeArray(){
-
-    try {
-        // convert JSON object to a string
-        const arrjson = JSON.stringify(arr, null, 4)
-    
-        // write file to disk
-        fs.writeFileSync('./playerDetailsArray.json', arrjson, 'utf8')
-    
-        console.log(`File is written successfully!`)
-    } catch (err) {
-        console.log(`Error writing file: ${err}`)
-    }
-}
+main()
